f24d3991197d8972e4ba43824b72ae9e098ee3e8
elvina2023 <142301271+elvina2023@users.noreply.github.com>
2023-11-29 10:18:17 +0700

    mobile app BansosPlus pt1 UI
    login, regis done

---
+++ BansosPlus/app/src/main/java/com/dicoding/bansosplus/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.bansosplus.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.bansosplus.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
