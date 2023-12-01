14cdc924e3f35d161906eacb2bbfc448393716b5
Vinz1403 <142301271+elvina2023@users.noreply.github.com>
2023-12-01 09:07:11 +0700

    home page part 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+//    card view no recycle
+    implementation ("androidx.cardview:cardview:1.0.0")
+
 
 }
