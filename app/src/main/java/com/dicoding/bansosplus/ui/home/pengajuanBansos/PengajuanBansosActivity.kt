8b3d08cfb2a79a559e45ee2e6b88bf903aa33298
Vinz1403 <142301271+elvina2023@users.noreply.github.com>
2023-12-02 15:52:30 +0700

    home page done

---
+++ app/src/main/java/com/dicoding/bansosplus/ui/home/pengajuanBansos/PengajuanBansosActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.bansosplus.ui.home.pengajuanBansos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.bansosplus.R
+
+class PengajuanBansosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pengajuan_bansos)
+    }
+}
