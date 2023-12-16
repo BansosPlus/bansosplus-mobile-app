db43d6434a8b7c9b64666ee8c069ec3a8243b77d
Vinz1403 <142301271+elvina2023@users.noreply.github.com>
2023-12-16 17:45:22 +0700

    feedback activity done, no backend

---
+++ app/src/main/java/com/dicoding/bansosplus/navigation/views/history/detailHistory/DetailHistoryViewModel.kt
@@ -0,0 +1,61 @@
+package com.dicoding.bansosplus.navigation.views.history.detailHistory
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dicoding.bansosplus.SessionManager
+import com.dicoding.bansosplus.navigation.data.model.BansosItem
+import com.dicoding.bansosplus.navigation.data.model.FeedbackItem
+import com.dicoding.bansosplus.repository.BansosRepository
+import com.dicoding.bansosplus.repository.FeedbackRepository
+import kotlinx.coroutines.launch
+
+class DetailHistoryViewModel (
+    private val sessionManager: SessionManager,
+) : ViewModel() {
+    private val bansosRepository: BansosRepository = BansosRepository(sessionManager)
+    private val feedbackRepository: FeedbackRepository = FeedbackRepository(sessionManager)
+    private val _bansosItem = MutableLiveData<BansosItem?>()
+    private val _feedbackList = MutableLiveData(ArrayList<FeedbackItem>())
+    val bansosItem: MutableLiveData<BansosItem?>
+        get() = _bansosItem
+    val feedbackList: LiveData<ArrayList<FeedbackItem>>
+        get() = _feedbackList
+
+    fun get(id: String) {
+        viewModelScope.launch {
+            try {
+                val response = bansosRepository.getDetail(id)
+                if (response.isSuccessful) {
+                    val item = response.body()?.data
+                    _bansosItem.value = item
+                    Log.i("BANSOS", "Get detail bansos successfully")
+                } else {
+                    Log.e("BANSOS", "Response failed")
+                }
+            } catch (e: Exception) {
+                Log.e("BANSOS", "Connection failed")
+            }
+        }
+    }
+
+    fun getFeedback(id: Int) {
+        viewModelScope.launch {
+            try {
+                val response = feedbackRepository.getFeedbackByBansos(id.toString())
+                if (response.isSuccessful) {
+                    val list = response.body()?.data
+                    _feedbackList.value = list as ArrayList<FeedbackItem>
+                    Log.i("FEEDBACK", "Get list of feedback successfully $list")
+                } else {
+                    Log.e("FEEDBACK", "Response failed")
+                }
+            } catch (e: Exception) {
+                Log.e("FEEDBACK", "Connection failed")
+            }
+
+        }
+    }
+}
